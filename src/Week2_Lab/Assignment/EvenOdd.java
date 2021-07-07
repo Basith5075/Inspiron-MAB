0404f00c98a457354731541a5820caf428403980
Abdul Basith <basith.5075@gmail.com>
2021-07-08 00:50:00 +0530

    Removed unnecessary comments from EvenOdd.java

--- src/Week2_Lab/Assignment/EvenOdd.java
+++ src/Week2_Lab/Assignment/EvenOdd.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class EvenOdd {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please Enter a number:");
 		int num = scan.nextInt();
