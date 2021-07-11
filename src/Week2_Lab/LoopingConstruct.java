4ddf1876bec2f5d67e8f63bf79dcf6664b5520bd
Abdul Basith <basith.5075@gmail.com>
2021-07-11 09:34:17 +0530

    11-07-21 Committing complex for loop.

---
+++ src/Week2_Lab/LoopingConstruct.java
@@ -0,0 +1,11 @@
+package Week2_Lab;
+
+public class LoopingConstruct {
+	public static void main(String[] args) {
+		
+		for(int i=10, j=1; i>0 &&j<=10; i--,j++) {
+			System.out.println(i+" "+j);
+		}
+	}
+
+}
